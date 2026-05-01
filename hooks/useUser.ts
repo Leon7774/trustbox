95c7a3dc22c9717ff5636cfaeac9975bd037c6c9
Leon <galileon.destura@gmail.com>
2026-05-01 17:54:42 +0800

    feat: add useUser hook to grab user context

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,8 +1,11 @@
 import { useContext } from "react";
 import { User } from "@supabase/supabase-js";
-import { UserContext } from "@/components/providers/UserProvider";
+import {
+  UserContext,
+  UserContextType,
+} from "@/components/providers/UserProvider";
 
-export function useUser() {
+export function useUser(): UserContextType {
   const context = useContext(UserContext);
   if (context === undefined) {
     throw new Error("useUser must be used within a UserProvider");
